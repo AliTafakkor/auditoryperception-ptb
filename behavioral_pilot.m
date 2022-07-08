7a2880cf9c630341cc9eb657d61a9d2e6a3e33e8
ali <atafakko@uwo.ca>
2022-07-08 17:20:06 -0400

    added some verbose

--- behavioral_pilot.m
+++ behavioral_pilot.m
@@ -6,13 +6,19 @@ familiarization = input('Session include familiarization (y/n): ', 's');
 
 % Run session
 if (strcmp(familiarization, 'y'))
-    
+    fprintf('starting familiarization section:')
     stimuli_familiarization;
+    fprintf('starting recognition task:')
     stimuli_recognition;
+    fprintf('starting imageability section:')
     stimuli_imageability;
+    fprintf('End of the session.')
 elseif (strcmp(familiarization, 'n'))
+    fprintf('starting recognition task:')
     stimuli_recognition;
+    fprintf('starting imageability section:')
     stimuli_imageability;
+    fprintf('End of the session.')
 else
     error('Enter valid response (y or n)!')
 end
